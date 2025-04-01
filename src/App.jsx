380289c27dbba5ec8753686e10055ae4b2e459a5
sumit <sumitkumar.verma@kreatio.com>
2025-04-01 15:30:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './App.css'
+import Navbar from "./components/Navbar";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './components/pages/Home';
+import Services from "./components/pages/Services.jsx";
+import Products from "./components/pages/Products.jsx";
+import SignUp from "./components/pages/SignUp.jsx";
+import Footer from "./components/Footer.jsx";
+
+function App() {
+
+    return (
+        <>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/services' element={<Services/>}/>
+                    <Route path='/products' element={<Products/>}/>
+                    <Route path='/sign-up' element={<SignUp/>}/>
+                </Routes>
+                <Footer />
+            </Router>
+        </>
+    )
+}
+
+export default App
